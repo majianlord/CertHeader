0cb99606c38d18193e9abb3706bfe09e34fcadd5
Christopher Hood <majianlord@gmail.com>
2022-07-31 19:06:53 -0500

    Add project files.

---
+++ CertHeaderMap/CertHeaderMap.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using CertHeader.Logging;
+using NLog;
+
+
+namespace CertHeader
+{
+    public class CertHeaderMap : IHttpModule
+    {
+        public CertHeaderMap()
+        {
+        }
+
+        public String ModuleName
+        {
+            get { return "CertHeaderMap"; }
+        }
+
+
+        // In the Init function, register for HttpApplication 
+        // events by adding your handlers.
+        public void Init(HttpApplication application)
+        {
+            
+            Log.LoggerSetup(NLog.LogLevel.Info, false);
+            application.BeginRequest +=
+                (new EventHandler(this.Application_BeginRequest));
+            application.EndRequest +=
+                (new EventHandler(this.Application_EndRequest));
+        }
+
+        private void Application_BeginRequest(Object source, EventArgs e)
+        {
+            //Log.LoggerSetup(NLog.LogLevel.Info, false);
+            HttpApplication application = (HttpApplication)source;
+            HttpContext context = application.Context;
+            HttpContext current = HttpContext.Current;
+
+            HttpClientCertificate theHttpCertificate = HttpContext.Current.Request.ClientCertificate;
+
+           
+            if (theHttpCertificate != null)
+            {
+                string EDIPI = theHttpCertificate.Subject.ToString().Right(10);
+                Log.Instance.Info(ModuleName + " : " + theHttpCertificate.Subject.ToString().Right(10));
+                if (EDIPI != null)
+                {
+                    current.Request.Headers.Add("EDIPI", EDIPI);
+                }
+            }
+            else
+            {
+                Log.Instance.Info(ModuleName + " : No CERT Persented");
+            }
+        }
+
+        private void Application_EndRequest(Object source, EventArgs e)
+        {
+            //HttpApplication application = (HttpApplication)source;
+            //HttpContext context = application.Context;
+            //string filePath = context.Request.FilePath;
+            //string fileExtension =
+            //    VirtualPathUtility.GetExtension(filePath);
+            //if (fileExtension.Equals(".aspx"))
+            //{
+            //    context.Response.Write("<hr><h1><font color=red>" +
+            //        "HelloWorldModule: End of Request</font></h1>");
+            //}
+        }
+
+
+
+        public void Dispose() { }
+    }
+
+}
---
+++ CertHeaderMap/Logging.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace CertHeader.Logging
+{
+    internal static class Log
+    {
+        public static Logger Instance { get; private set; }
+        static Log()
+        {
+            LogManager.ReconfigExistingLoggers();
+            Instance = LogManager.GetCurrentClassLogger();
+        }
+
+        public static void LoggerSetup(NLog.LogLevel LogLevel, bool timings)
+        {
+            // If LogManager.Configuration Is Nothing Then
+            LoggingConfiguration LoggingConfig = new LoggingConfiguration();
+            // Setup Default File Log
+            FileTarget FileTarget = new FileTarget();
+            FileTarget.FileName = "${basedir}/log/logfile.log";
+            FileTarget.Layout = "${longdate} : ${uppercase:${level}} : ${message} ${exception:format=tostring}";
+            FileTarget.Name = "FileTarget";
+            FileTarget.ArchiveFileName = "${basedir}/log/archives/log.{#}.log";
+            FileTarget.ArchiveEvery = FileArchivePeriod.Hour;
+            FileTarget.ArchiveNumbering = ArchiveNumberingMode.Date;
+            FileTarget.MaxArchiveFiles = 168;
+            LoggingConfig.AddTarget(FileTarget);
+            LoggingRule Rule1 = new LoggingRule("*", LogLevel, FileTarget);
+            LoggingConfig.LoggingRules.Add(Rule1);
+            LogManager.Configuration = LoggingConfig;
+        }
+
+    }
+}
+
+
+
+
